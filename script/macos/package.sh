aabc73f282a93d80aea5fa300263df9443b345dd
codeCLown <zhaomengshou@126.com>
2026-06-01 18:13:21 +0800

    update

--- script/macos/package.sh
+++ script/macos/package.sh
@@ -6,6 +6,19 @@ PUBLISH_OUTPUT_DIRECTORY="../../LunaTV/bin/Release/net10.0/osx-$arch/publish/."
 INFO_PLIST="./Info.plist"
 ICON_FILE="./logo.icns"
 
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/*.pdb")
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/de")
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/es")
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/fr")
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/it")
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/ja")
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/ko")
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/pt-BR")
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/ru")
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/zh-Hans")
+rm -rf $(find . -name "../../LunaTV/bin/Release/net10.0/osx-$arch/publish/zh-Hant")
+
+
 if [ -d "$APP_NAME" ]; then
   rm -rf "$APP_NAME"
 fi
